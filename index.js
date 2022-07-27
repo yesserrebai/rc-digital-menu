a504e1896a9520dc5f6e7d885981deb0dbbb89aa
yesserrebai <80044980+yesserrebai@users.noreply.github.com>
2022-07-27 17:02:52 +0100

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import path from 'path';
+const app = express();
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+app.listen(4000, () => {
+  console.log('listenning on 4000');
+});
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, './views'));
+app.use(express.static(path.join(__dirname, 'public')));
+try {
+  app.get('/:name', (req, res) => {
+    let restoName = req.params.name;
+    return res.status(200).render('index');
+  });
+} catch (e) {
+  console.log(e);
+}
